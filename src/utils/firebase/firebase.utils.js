493dfab481b688d09a785c78b86cdac58965c98b
githubdoesntgivemeausername <aakashsukumar@gmail.com>
2022-10-19 22:51:53 +0530

    adding sign up component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -19,23 +19,28 @@ const firebaseConfig = {
 
   const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
   
-  provider.setCustomParameters({
-    prompt: 'select_account'
-  });
-  
-  export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-  
-  export const db = getFirestore();
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
   
   export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
-    //console.log(userDocRef);
+
     const userSnapshot = await getDoc(userDocRef);
-   // console.log(userSnapshot);
-   // console.log(userSnapshot.exists());
+
   
     if (!userSnapshot.exists()) {
       const { displayName, email } = userAuth;
